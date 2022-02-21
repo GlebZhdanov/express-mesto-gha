be62801d3a4a4fbb9596f6daef406c1740baede0
Gleb Zhdanov <zhdanov-gleb@mail.ru>
2022-02-21 22:24:22 +0300

    fix: Второе ревью

--- controllers/users.js
+++ controllers/users.js
@@ -41,19 +41,15 @@ module.exports.postUsers = async (req, res) => {
 
 module.exports.patchUsers = async (req, res) => {
   try {
-    // const { name, about } = req.body;
+    const { name, about } = req.body;
     const user = await User.findByIdAndUpdate(
       req.user._id,
-      { $set: {
-          name : req.body.name,
-          about : req.body.about
-        }
-      },
+      { name, about },
       { new: true, runValidators: true },
     );
      if (user) {
       return res.status(200).send(user);
-    } if (!req.body.name || !req.body.about) {
+    } if (!name || !about) {
       return res.status(400).send({ message: 'Поля "name" и "about" должно быть заполнены' });
     }
     return res.status(404).send({ message: 'Пользователь не найден' });
